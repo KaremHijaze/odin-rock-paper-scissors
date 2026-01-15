b22efe74d62ab97394b4649f468002626c2a1de1
Karem Hijaze <126591818+KaremHijaze@users.noreply.github.com>
2026-01-15 21:03:03 +0200

    Added a new function for taking the users' choice
    added a function that prompts the user inorder to
    get his playing choice (rock, paper or scissors) for
    later comparison with computer choice according to
    the game's logic.

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-console.log("Hello world!");
+console.log("- Welcome to Rock Paper Scissors -");
+
 const choices = ['rock', 'paper', 'scissors'];
+let humanScore  = 0, computerScore =0;
+
+// returns a random choice from the choices 
 const getComputerChoice = function(choices) {
-    const choiceIndex =  Math.floor(Math.random()*choices.length);
-    return choices[choiceIndex];
+    return choices[Math.floor(Math.random()*choices.length)];
 };
 
+// prompts the user input then returns it
+const getHumanChoice = function() {
+    return prompt("Choose Rock / Paper / Scissors: ").toLowerCase();
+};
+
+//! Testing funcitons
 console.log(getComputerChoice(choices));
+console.log(getHumanChoice());
