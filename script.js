ad8761d35a92fb7ef8dad0bbb9f8180ed006a874
Karem Hijaze <126591818+KaremHijaze@users.noreply.github.com>
2026-01-15 21:39:57 +0200

    Added the logic for a single round play
    added variabels to keep track of the scores and a
    one-round function implementation that takes the
    computer and user choices then updates the scores
    and prints the winner of the round.

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const choices = ['rock', 'paper', 'scissors'];
 let humanScore  = 0, computerScore =0;
 
 // returns a random choice from the choices 
-const getComputerChoice = function(choices) {
-    return choices[Math.floor(Math.random()*choices.length)];
+const getComputerChoice = function() {
+    return choices[Math.floor(Math.random() * choices.length)];
 };
 
 // prompts the user input then returns it
@@ -13,6 +13,46 @@ const getHumanChoice = function() {
     return prompt("Choose Rock / Paper / Scissors: ").toLowerCase();
 };
 
+// takes human and computer choice and updates score accordingly
+const playRound = function(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return;
+    }
+
+    if (humanChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            computerScore++;
+            console.log("You Lose! Paper beats Rock.");
+        }
+        else {
+            humanScore++;
+            console.log("You Win! Paper beats Rock.");
+        }
+    }
+
+    else if (humanChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            computerScore++;
+            console.log("You Lose! Scissors beats Papper.");
+        }
+        else {
+            humanScore++;
+            console.log("You Win! Paper beats Rock.");
+        }
+    }
+
+    else {
+        if (computerChoice === 'rock') {
+            computerScore++;
+            console.log("You Lose! Rock beats Scissors.");
+        }
+        else {
+            humanScore++;
+            console.log("You Win! Scissors beats Paper.");
+        }
+    }
+}; 
+
 //! Testing funcitons
-console.log(getComputerChoice(choices));
+console.log(getComputerChoice());
 console.log(getHumanChoice());
