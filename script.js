5ce257ba1c4cc1c0dc8fd952d1cbc089f72e31c2
Karem Hijaze <126591818+KaremHijaze@users.noreply.github.com>
2026-01-15 20:43:19 +0200

    Correct repo name and add computer choice func
    corrected the repo's name along side the mispells
    in the html file, then added and tested the funciton
    called getComputerChoice which when given an array
    of choices it returns one of them randomly.

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello world!");
+const choices = ['rock', 'paper', 'scissors'];
+const getComputerChoice = function(choices) {
+    const choiceIndex =  Math.floor(Math.random()*choices.length);
+    return choices[choiceIndex];
+};
+
+console.log(getComputerChoice(choices));
